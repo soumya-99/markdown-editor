9132fb753ced8329be0d3edaa7f28307245f4fb0
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-12-10 14:04:57 +0530

    Added firebase hosting

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyAum4P04XjaQj-SdjVAWfsU0kpgBIerBvU",
+	authDomain: "markdown-editor-a139b.firebaseapp.com",
+	projectId: "markdown-editor-a139b",
+	storageBucket: "markdown-editor-a139b.appspot.com",
+	messagingSenderId: "960903614795",
+	appId: "1:960903614795:web:7c4b1a07b9c306f6f998ce",
+	measurementId: "G-NMFNLZ3X7K",
+};
+
+firebase.initializeApp(firebaseConfig);
